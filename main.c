a503a08bfcb18abcbdbe63427f30008ec85baf09
Mohammad Alkhatib <mohammad.alkhatib@web.de>
2024-12-20 23:15:52 +0100

    First working commit, will draw a blue-ish rect on the desktop.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <Windows.h>
 #include <stdio.h>
 
-HWND findDesktopWindow(void);
+HWND GetWallpaperArea(void);
 
 BOOL CALLBACK EnumProc(HWND hwnd, LPARAM lParam);
 
@@ -10,24 +10,31 @@ LRESULT CALLBACK WindowProc(HWND hwnd, long uMsg, WPARAM wParam, LPARAM lParam);
 int main(void) {
     printf("Starting...\n");
 
-    HWND desktop = findDesktopWindow();
+    HWND desktop = GetWallpaperArea();
     if (!desktop) {
-        printf("Failed to find desktop window handle\n");
+        printf("Unable to get wallpaper area\n");
         return 1;
     }
 
     HDC hdc = GetDC(desktop);
-
     if (!hdc) {
         printf("Failed to get graphics device context\n");
         return 1;
     }
 
     HBRUSH hbr = CreateSolidBrush(RGB(0, 30, 130));
+    if (!hbr) {
+        printf("Failed to create brush\n");
+        ReleaseDC(desktop, hdc);
+        return 1;
+    }
+
     SelectObject(hdc, hbr);
 
+    RECT rect = {50, 50, 500, 500};
+
     while ((GetAsyncKeyState(VK_ESCAPE) & 0x8000) == 0) {
-        Rectangle(hdc, 50, 50, 500, 500);
+        FillRect(hdc, &rect, hbr);
         Sleep(16);
     }
 
@@ -36,29 +43,41 @@ int main(void) {
     return 0;
 }
 
-HWND findDesktopWindow() {
-    HWND progman = GetShellWindow();
-
-    SendMessageTimeoutA(progman, 0x052C, 0, 0, SMTO_NORMAL, 1000, NULL);
-
-    HWND wallpaper_hwnd = NULL;
-    EnumWindows(EnumProc, (LPARAM)&wallpaper_hwnd);
-
-    if (!wallpaper_hwnd)
+HWND GetWallpaperArea() {
+    HWND desktop = GetDesktopWindow();
+    if (!desktop) {
+        printf("Failed to find desktop window handle\n");
         return NULL;
+    }
 
-    return wallpaper_hwnd;
+    HWND listview = NULL;
+    EnumWindows(EnumProc, (LPARAM) &listview);
+    if (!listview) {
+        printf("Failed to find window\n");
+        return NULL;
+    }
+
+    return listview;
 }
 
 BOOL CALLBACK EnumProc(HWND hwnd, LPARAM lParam) {
+    HWND *parentHandle = (HWND *) lParam;
     char className[256];
     if (GetClassName(hwnd, className, sizeof(className))) {
         if (strcmp(className, "WorkerW") == 0) {
-            *(HWND*)lParam = hwnd;
-            return FALSE;
+            HWND child = FindWindowEx(hwnd, NULL, "SHELLDLL_DefView", NULL);
+            if (child != NULL) {
+                *parentHandle = hwnd;
+                HWND sysListView = FindWindowEx(child, NULL, "SysListView32", "FolderView");
+                if (sysListView != NULL) {
+                    printf("Found SysListView32 window!\n");
+                    *(HWND *) lParam = sysListView;
+                    return FALSE;
+                }
+            }
         }
     }
-    return TRUE;
+    return TRUE; // Continue enumeration
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, long uMsg, WPARAM wParam, LPARAM lParam) {
