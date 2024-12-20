2bd9f544eedba83f08bf0d26c29d91a753e71c4d
Mohammad Alkhatib <mohammad.alkhatib@web.de>
2024-12-20 22:46:06 +0100

    Introduced framework to render, though it's not functional right now.

--- main.c
+++ main.c
@@ -1,6 +1,66 @@
 #include <Windows.h>
 #include <stdio.h>
 
+HWND findDesktopWindow(void);
+
+BOOL CALLBACK EnumProc(HWND hwnd, LPARAM lParam);
+
+LRESULT CALLBACK WindowProc(HWND hwnd, long uMsg, WPARAM wParam, LPARAM lParam);
+
+int main(void) {
+    printf("Starting...\n");
+
+    HWND desktop = findDesktopWindow();
+    if (!desktop) {
+        printf("Failed to find desktop window handle\n");
+        return 1;
+    }
+
+    HDC hdc = GetDC(desktop);
+
+    if (!hdc) {
+        printf("Failed to get graphics device context\n");
+        return 1;
+    }
+
+    HBRUSH hbr = CreateSolidBrush(RGB(0, 30, 130));
+    SelectObject(hdc, hbr);
+
+    while ((GetAsyncKeyState(VK_ESCAPE) & 0x8000) == 0) {
+        Rectangle(hdc, 50, 50, 500, 500);
+        Sleep(16);
+    }
+
+    ReleaseDC(desktop, hdc);
+
+    return 0;
+}
+
+HWND findDesktopWindow() {
+    HWND progman = GetShellWindow();
+
+    SendMessageTimeoutA(progman, 0x052C, 0, 0, SMTO_NORMAL, 1000, NULL);
+
+    HWND wallpaper_hwnd = NULL;
+    EnumWindows(EnumProc, (LPARAM)&wallpaper_hwnd);
+
+    if (!wallpaper_hwnd)
+        return NULL;
+
+    return wallpaper_hwnd;
+}
+
+BOOL CALLBACK EnumProc(HWND hwnd, LPARAM lParam) {
+    char className[256];
+    if (GetClassName(hwnd, className, sizeof(className))) {
+        if (strcmp(className, "WorkerW") == 0) {
+            *(HWND*)lParam = hwnd;
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+
 LRESULT CALLBACK WindowProc(HWND hwnd, long uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_CLOSE:
@@ -12,72 +72,5 @@ LRESULT CALLBACK WindowProc(HWND hwnd, long uMsg, WPARAM wParam, LPARAM lParam) 
         default:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-    return 0;
-}
-
-int main(void) {
-    printf("Starting...\n");
-
-    WNDCLASSEX wc = {};
-    wc.cbSize = sizeof(WNDCLASSEX);
-    wc.hInstance = GetModuleHandle(NULL);
-    wc.style = 0;
-    wc.lpfnWndProc = WindowProc;
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.lpszClassName = "SomeSimpleWindowClass";
-
-    if (!RegisterClassEx(&wc)) {
-        printf("Failed to register the window class\n");
-        return 1;
-    }
-
-    HWND hwnd = CreateWindowEx(
-        WS_EX_CLIENTEDGE,
-        "SomeSimpleWindowClass",
-        "Title of the Window",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        800, 600,
-        NULL, NULL,
-        wc.hInstance,
-        NULL
-    );
-
-    if (!hwnd) {
-        printf("Failed to create the window\n");
-        UnregisterClass(wc.lpszClassName, wc.hInstance);
-        return 1;
-    }
-
-    HDC hdc = GetDC(hwnd);
-
-    if (!hdc) {
-        printf("Failed to get graphics device context\n");
-        DestroyWindow(hwnd);
-        UnregisterClass(wc.lpszClassName, wc.hInstance);
-        return 1;
-    }
-
-    HBRUSH hbr = CreateSolidBrush(RGB(0, 30, 130));
-    SelectObject(hdc, hbr);
-
-    ShowWindow(hwnd, SW_SHOW);
-    UpdateWindow(hwnd);
-
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-
-        Rectangle(hdc, 50, 50, 500, 500);
-        Sleep(16);
-
-    }
-
-    ReleaseDC(hwnd, hdc);
-    DestroyWindow(hwnd);
-    UnregisterClass(wc.lpszClassName, wc.hInstance);
-
     return 0;
 }
